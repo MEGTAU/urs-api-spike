7f35095d8c7da96fcf5115d98eda2e80d0b66564
dougwright <dougwright@megt.com.au>
2025-09-12 19:37:16 +1000

    Add a test to add a contact

---
+++ api_spec_workspace/urs-api-specs/src/test/contacts.test.js
@@ -0,0 +1,30 @@
+const supertest = require('supertest');
+const { expect } = require('chai');
+const ExpressServer = require('../expressServer');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
+// The openapi.yaml is in the api directory, so we need to construct the path to it.
+const openApiYaml = path.join(__dirname, '../api/openapi.yaml');
+const server = new ExpressServer(8081, openApiYaml);
+const request = supertest(server.app);
+
+describe('Contacts API', () => {
+  it('should create a new contact', async () => {
+    const newContact = {
+      firstName: 'John',
+      lastName: 'Doe',
+    };
+
+    const res = await request
+      .post('/api/v1/contacts')
+      .send(newContact)
+      .expect(201);
+
+      expect(res.body).to.be.an('object');
+      let id = res.body.id;
+      expect(id).to.be.a('string');
+    expect(res.body.firstName).to.equal(newContact.firstName);
+    expect(res.body.lastName).to.equal(newContact.lastName);
+  });
+});
