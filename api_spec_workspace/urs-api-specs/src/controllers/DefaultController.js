9cd2c9b476393c8f085634297d7523ffaeda40ce
dougwright <dougwright@megt.com.au>
2025-09-12 16:34:21 +1000

    Split relationships API methods into separate controller and service implementations

--- api_spec_workspace/urs-api-specs/src/controllers/DefaultController.js
+++ api_spec_workspace/urs-api-specs/src/controllers/DefaultController.js
@@ -9,38 +9,7 @@ const interactionsPOST = async (request, response) => {
   await Controller.handleRequest(request, response, service.interactionsPOST);
 };
 
-const relationshipsBulkPUT = async (request, response) => {
-  await Controller.handleRequest(request, response, service.relationshipsBulkPUT);
-};
-
-const relationshipsGET = async (request, response) => {
-  await Controller.handleRequest(request, response, service.relationshipsGET);
-};
-
-const relationshipsPOST = async (request, response) => {
-  await Controller.handleRequest(request, response, service.relationshipsPOST);
-};
-
-const relationshipsRelationshipIdDELETE = async (request, response) => {
-  await Controller.handleRequest(request, response, service.relationshipsRelationshipIdDELETE);
-};
-
-const relationshipsRelationshipIdGET = async (request, response) => {
-  await Controller.handleRequest(request, response, service.relationshipsRelationshipIdGET);
-};
-
-const relationshipsRelationshipIdPUT = async (request, response) => {
-  await Controller.handleRequest(request, response, service.relationshipsRelationshipIdPUT);
-};
-
-
 module.exports = {
   interactionsBulkPOST,
   interactionsPOST,
-  relationshipsBulkPUT,
-  relationshipsGET,
-  relationshipsPOST,
-  relationshipsRelationshipIdDELETE,
-  relationshipsRelationshipIdGET,
-  relationshipsRelationshipIdPUT,
 };
